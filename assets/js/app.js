22b79affb90c95e986bb4562b781a9dd521f46f9
manucarenjot <carenjotje@gmail.com>
2021-11-19 11:59:32 +0100

    Deviner un nombre

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,18 @@
 let fenetre = document.getElementById("window");
 let text = document.getElementById("text");
 let button = document.getElementById("submit");
+let chances = document.getElementById("chances");
 
-let i;
 let reponse = Math.floor (Math.random() * 100) +1;
 console.log(reponse)
+
+
+let i = 0
+while (i<10) {
+    console.log(i)
+    i++
+}
+
 
 
 
@@ -12,7 +20,6 @@ console.log(reponse)
 
 button.addEventListener("click", function(){
     console.log('Boum');
-    for (i=0; i < 10; i++);
     let test = document.createElement("p");
     test.innerHTML = text.value;
     test.style.backgroundColor = "white";
@@ -23,19 +30,26 @@ button.addEventListener("click", function(){
     if (parseInt(text.value)===reponse) {
 
         test.innerHTML = text.value + " | You win";
+        test.style.backgroundColor = "green";
     }
 
     else if (parseInt(text.value) < reponse) {
         test.innerHTML = text.value + " ||  TON NOMBRE EST TROP PETIT QUEL DOMMAGE !";
+        test.style.backgroundColor = "red";
     }
 
     else if (parseInt(text.value) > reponse) {
         test.innerHTML = text.value + " ||  TON NOMBRE EST TROP GRAND MALHEUREUX !";
+        test.style.backgroundColor = "red";
+        test.style.color = "white";
     }
 
     else if (parseInt(text.value) !== reponse) {
         test.innerHTML = text.value + " ||  ON A DIT DES NOMBRE PAS DES LETTRES, TU ME FAIS DOUTER DE TES CAPACITE !";
+        test.style.backgroundColor = "red";
+        test.style.color = "white";
     }
+
 
 
 });
