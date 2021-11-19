53566fa569999889eea4ecae08dc5cdf1da95242
manucarenjot <carenjotje@gmail.com>
2021-11-19 14:26:48 +0100

    Deviner un nombre

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,7 +3,7 @@ let text = document.getElementById("text");
 let button = document.getElementById("submit");
 let chances = document.getElementById("chances");
 let dialogue = document.getElementById("erreur");
-let number = document.getElementById("les-nombres");
+let number = document.getElementById("number");
 let reponse = Math.floor(Math.random() * 100) + 1;
 console.log(reponse)
 
@@ -22,35 +22,46 @@ function clic() {
 
             dialogue.innerHTML ="C'est gagné ! OUI!";
             dialogue.style.color = "rgba(0,255,4,0.7)";
-            dialogue.style.fontSize = "2.3rem"
+            dialogue.style.fontSize = "2.3rem";
             dialogue.style.textAlign = "center"
-            dialogue.style.backgroundColor = "rgba(63,115,140,0.7)"
+            dialogue.style.backgroundColor = "rgba(63,115,140,0.7)";
+            number.innerText += text.value + " | "
+            number.style.fontSize = "3rem"
+            number.style.color = "rgba(0,255,4,0.7)"
         } else if (parseInt(text.value) < reponse) {
+           dialogue.innerHTML = " TON NOMBRE EST TROP PETIT QUEL DOMMAGE !";
+            dialogue.style.color = "red";
+            dialogue.style.fontSize = "1.3rem"
+            dialogue.style.textAlign = "center"
+            dialogue.style.backgroundColor = "rgba(91,255,255,0.7)"
+            number.innerText += text.value + " | "
+            number.style.fontSize = "3rem"
 
-           dialogue.innerHTML = text.value + " ||  TON NOMBRE EST TROP PETIT QUEL DOMMAGE !";
-            dialogue.style.backgroundColor = "red";
-            let nombre =document.createElement("p");
-            number.append(nombre);
-            nombre.innerHTML = text.value
         } else if (parseInt(text.value) > reponse) {
             dialogue.innerHTML ="TON NOMBRE EST TROP GRAND MALHEUREUX !";
             dialogue.style.color = "red";
             dialogue.style.fontSize = "1.3rem"
             dialogue.style.textAlign = "center"
             dialogue.style.backgroundColor = "rgba(91,255,255,0.7)"
+            number.innerText += text.value + " | "
+            number.style.fontSize = "3rem"
         } else if (parseInt(text.value) !== reponse) {
             dialogue.innerHTML = "  ON A DIT DES NOMBRE PAS DES LETTRES, TU ME FAIS DOUTER DE TES CAPACITE !";
             dialogue.style.color = "red";
             dialogue.style.fontSize = "1.3rem"
             dialogue.style.textAlign = "center"
             dialogue.style.backgroundColor = "rgba(100,215,215,0.7)"
+            number.innerText += text.value + " | "
+            number.style.fontSize = "3rem"
         }
         if (i===11) {
             dialogue.innerHTML ="  FELICITATION ! TU AS PERDU ! ";
             dialogue.style.color = "red";
-            dialogue.style.fontSize = "1.3rem"
-            dialogue.style.textAlign = "center"
-            dialogue.style.backgroundColor = "rgba(91,255,255,0.7)"
+            dialogue.style.fontSize = "1.3rem";
+            dialogue.style.textAlign = "center";
+            dialogue.style.backgroundColor = "rgba(91,255,255,0.7)";
+            fenetre.style.animationName = "error";
+            fenetre.style.animationDuration = "0.5s";
         }
 
 
